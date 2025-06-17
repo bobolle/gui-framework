48c0f9badbb16c053a706e994f2df165bff2bcbf
bobolle <botdahlstrom@gmail.com>
2025-06-17 21:06:08 +0200

    feat: add text renderer

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,17 @@ extern "C" {
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <map>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Shader.hpp"
+#include "TextShader.hpp"
 #include "Widget.hpp"
 #include "Layout.hpp"
 
@@ -76,22 +83,6 @@ static GLFWwindow* initWindow()
     return window;
 }
 
-static void renderText(const std::string& text, float x, float y)
-{
-    printf("%s\n", text);
-}
-
-int lua_renderText(lua_State* L)
-{
-    const std::string text = luaL_checkstring(L, 1);
-    float x = static_cast<float>(luaL_checknumber(L, 2));
-    float y = static_cast<float>(luaL_checknumber(L, 3));
-
-    renderText(text, x, y);
-
-    return 0;
-}
-
 class Rectangle : public Widget
 {
         using Widget::Widget;
@@ -117,30 +108,134 @@ void Triangle::render(unsigned int height)
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
+struct Character {
+    GLuint TextureID;
+    glm::ivec2 Size;
+    glm::ivec2 Bearing;
+    GLuint Advance;
+};
+
+std::map<GLchar, Character> Characters;
+GLuint tVAO, tVBO;
+
+void renderText(TextShader* s, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+    s->use();
+    glUniformMatrix4fv(glGetUniformLocation(s->ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    glUniform3f(glGetUniformLocation(s->ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(tVAO);
+
+    for (auto c : text)
+    {
+        Character ch = Characters[c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+
+        float vertices[6][4] = {
+            { xpos,     ypos + h, 0.0f, 0.0f },
+            { xpos,     ypos,     0.0f, 1.0f },
+            { xpos + w, ypos,     1.0f, 1.0f },
+
+            { xpos,     ypos + h, 0.0f, 0.0f },
+            { xpos + w, ypos,     1.0f, 1.0f },
+            { xpos + w, ypos + h, 1.0f, 0.0f }
+        };
+
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        glBindBuffer(GL_ARRAY_BUFFER, tVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        x += (ch.Advance >> 6) * scale;
+    }
+
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 int main(void)
 {
-    int status;
-    lua_State *L;
+    // create window
+    GLFWwindow* window = initWindow();
 
-    // holds all lua contexts
-    L = luaL_newstate();
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // load lua libs
-    luaL_openlibs(L);
-
-    // load lua script file
-    status = luaL_loadfile(L, "script.lua");
-    if (status)
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
     {
-        std::cout << "Couldn't load file: " << lua_tostring(L, -1) << std::endl;
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
         exit(1);
     }
 
-    // register functions
-    lua_register(L, "renderText", lua_renderText);
+    FT_Face face;
+    if (FT_New_Face(ft, "fonts/Monaco_Linux.ttf", 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        exit(1);
+    }
 
-    // create window
-    GLFWwindow* window = initWindow();
+
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    for (GLubyte c = 0; c < 128; c++)
+    {
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            (GLuint)face->glyph->advance.x
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+
+
+    glGenVertexArrays(1, &tVAO);
+    glGenBuffers(1, &tVBO);
+    glBindVertexArray(tVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, tVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     // vertices
     float vertices[] = {
@@ -151,6 +246,7 @@ int main(void)
 
     // create Shader Program
     Shader shader;
+    TextShader text_shader;
 
     // Vertex Array Object and Vertex Buffer Object
     unsigned int VAO, VBO;
@@ -179,6 +275,7 @@ int main(void)
     {
         int width, height;
         glfwGetWindowSize(window, &width, &height);
+
         // render
         glScissor(0, 0, width, height);
         glViewport(0, 0, width, height);
@@ -192,14 +289,16 @@ int main(void)
 
         Rectangle rec(&layout.cursor, 100, 100, {r: 0.3f, g: 0.5f, b: 0.2f, a: 1.0f});
         Triangle tri(&layout.cursor, 100, 100, {r: 1.0f, g: 0.5f, b: 0.2f, a: 1.0f});
-        Triangle tri2(&layout.cursor, 100, 100, {r: 1.0f, g: 0.5f, b: 0.2f, a: 1.0f});
 
         layout.pushWidget(&rec);
         layout.pushWidget(&tri);
-        layout.pushWidget(&tri2);
 
         layout.handleInput(window);
         layout.render(height);
+
+        glScissor(0, 0, width, height);
+        glViewport(0, 0, width, height);
+        renderText(&text_shader, "example text", 25.0f, 25.0f, 1.0f, glm::vec3(0.9f, 0.8f, 0.2f));
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -210,7 +309,6 @@ int main(void)
 
     glfwDestroyWindow(window);
     glfwTerminate();
-    lua_close(L);
 
     return 0;
 }
